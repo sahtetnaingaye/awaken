2e7cebda38e9cceca142143909f9af9ada0e56fa
sahtetnaingaye <sa@revotech.co>
2015-04-07 19:39:30 +0630

    setup awaken April 7 2015 by salone

---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Awaken</title>
+        <link href="css/bootstrap.min.css" rel="stylesheet">
+        <link href="css/app.css" rel="stylesheet">
+    </head>
+    <body>
+        <div class="container-fluid h-bg">
+            <div class="col-lg-5 full-height br-white">
+                <div class="logo-text">
+                    
+                </div>
+                <p>design</p>
+            </div>
+            <div class="col-lg-5 full-height br-white">
+                <p>photography</p>
+            </div>
+            <div class="col-lg-5 full-height br-white">
+                <p>film</p>
+            </div>
+            <div class="col-lg-5 full-height br-white">
+                <p>more</p>
+            </div>
+            <div class="col-lg-5 full-height">
+                <p>about</p>
+            </div>
+        </div>
+    </body>
+</html>
